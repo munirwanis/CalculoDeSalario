282f33ebc2c1c01c52bd7864603acee5aa7abdab
Munir Wanis <fallen.angel.pp@gmail.com>
2017-08-23 12:38:52 -0300

    First Commit

---
+++ app/src/main/java/usuario/app/calculodesalario/SalarioActivity.java
@@ -0,0 +1,19 @@
+package usuario.app.calculodesalario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class SalarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_salario);
+    }
+
+    public void calcular(View view) {
+
+    }
+}
